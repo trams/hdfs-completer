c5c9f5d1eddd2bc499dd60674f48ccce23cb8ff8
Oleksandr Pryimak <oleksandr.pryimak@gmail.com>
2015-12-11 00:10:17 +0100

    Add HTTP interface

--- completer.py
+++ completer.py
@@ -1,5 +1,43 @@
-import hdfs.util
+#!/usr/bin/python
 
+import hdfs.util
+import tornado.web
+
+from tornado.options import define, options
+
+#############################################################
+
+class GetCompletetionsHandler(tornado.web.RequestHandler):
+    def __init__(self, application, request, state):
+        super(GetCompletetionsHandler, self).__init__(application, request)
+        self._state = state
+
+    def get(self):
+        path = self.get_argument("path")
+        completetions = get_completetions(self._state.root_node, path)
+        self.write("\n".join(completetions))
+
+
+class ReIndexHandler(tornado.web.RequestHandler):
+    def __init__(self, application, request, state):
+        super(ReIndexHandler, self).__init__(application, request)
+        self._state = state
+
+    def get(self):
+        max_depth = int(self.get_argument("max_depth", 4))
+        self._state.index(max_depth)
+
+
+class State(object):
+    def __init__(self):
+        self.root_node = None
+        self.client = None
+
+    def index(self, max_depth=5):
+        self.root_node = read_hdfs_tree(self.client, '', max_depth)
+
+
+#############################################################
 
 class Node(object):
     def __init__(self, name, is_folder, children):
@@ -13,6 +51,9 @@ def list_folder(client, path):
 
 
 def read_hdfs_tree(client, path, max_depth):
+    if max_depth == 0:
+        return {}
+
     real_path = path
     if real_path == "":
         real_path = "/"
@@ -39,16 +80,40 @@ def get_completetions(root_children, path):
     parts = filter(lambda x: x != '',  basename.split('/'))
 
     for name in parts:
+        if not current.has_key(name):
+            return []
         current = current[name].children
 
     return [basename + '/' + name for name in current if name.startswith(filename)]
 
+#############################################################
 
-if __name__ == "__main__":
+def get_client(host='http://192.168.33.10:50070'):
     from hdfs.client import Client
-    client = Client('http://192.168.33.10:50070')
+    return Client(host)
+
+
+def smoke_test():
     root_children = read_hdfs_tree(client, '', 7)
     print_hdfs_tree(root_children)
     print get_completetions(root_children, '/t')
     print get_completetions(root_children, '/tmp/t')
     print get_completetions(root_children, '/notexist')
+
+
+define("port", default=8888, help="port to listen")
+define("depth", default=5, help="max depth to index")
+define("hdfs_host", default="http://192.168.33.10:50070", help="hdfs host to index")
+
+
+if __name__ == "__main__":
+    tornado.options.parse_command_line()
+    state = State()
+    state.client = get_client(options.hdfs_host)
+    state.index(options.depth)
+    application = tornado.web.Application([
+        (r"/v1/completetions", GetCompletetionsHandler, dict(state=state)),
+        (r"/v1/reindex", ReIndexHandler, dict(state=state)),
+    ])
+    application.listen(options.port)
+    tornado.ioloop.IOLoop.current().start()
