8ca5040e52a619219071dac3cfd4bf169b80dc92
Oleksandr Pryimak <oleksandr.pryimak@gmail.com>
2016-01-22 18:19:36 +0100

    Make an hdfs completer endpoint customizable

--- hdfs_completer.sh
+++ hdfs_completer.sh
@@ -1,10 +1,13 @@
 
 
+HDFS_COMPLETER_HOST=${HDFS_COMPLETER_HOST-127.0.0.1}
+HDFS_COMPLETER_PORT=${HDFS_COMPLETER_PORT-8888}
+
 _complete_hdfs_path() {
     cur="${COMP_WORDS[COMP_CWORD]}"
 
     if [[ $cur == /* ]]; then
-        possible_completetions="`curl --silent 127.0.0.1:8888/v1/completetions?path=${cur}`"
+        possible_completetions="`curl --silent ${HDFS_COMPLETER_HOST}:${HDFS_COMPLETER_PORT}/v1/completetions?path=${cur}`"
         if test $? -eq 0; then
             COMPREPLY=($possible_completetions)
             return 0
