0bfbe48768a339dc8cde93fe118c5cd66f63e5cf
Oleksandr Pryimak <oleksandr.pryimak@gmail.com>
2016-02-14 14:56:43 +0100

    Specify a reasonable timeout

--- hdfs_completer.sh
+++ hdfs_completer.sh
@@ -6,7 +6,7 @@ _complete_hdfs_path() {
     cur="${COMP_WORDS[COMP_CWORD]}"
 
     if [[ $cur == /* ]]; then
-        possible_completetions="`curl --silent ${HDFS_COMPLETER_HOST}:${HDFS_COMPLETER_PORT}/v1/completetions?path=${cur}`"
+        possible_completetions="`curl --max-time 1 --silent ${HDFS_COMPLETER_HOST}:${HDFS_COMPLETER_PORT}/v1/completetions?path=${cur}`"
         if test $? -eq 0; then
             COMPREPLY=($possible_completetions)
             return 0
--- hdfs_list.sh
+++ hdfs_list.sh
@@ -2,4 +2,4 @@
 
 . $(dirname $0)/settings.sh
 
-curl ${HDFS_COMPLETER_HOST}:${HDFS_COMPLETER_PORT}/v1/list?path=$1
+curl --max-time 1 ${HDFS_COMPLETER_HOST}:${HDFS_COMPLETER_PORT}/v1/list?path=$1
