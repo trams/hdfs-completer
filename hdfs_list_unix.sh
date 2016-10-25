8a4c7d6492b112bd2b26ccbdcd27788fe996a00f
Oleksandr Pryimak <oleksandr.pryimak@gmail.com>
2016-10-25 14:57:34 +0200

    Make hdfs_list_unix more user friendly
    Add a message describing cluster's URL database

--- hdfs_list_unix.sh
+++ hdfs_list_unix.sh
@@ -18,7 +18,14 @@ readonly SOCKET_PATH="/tmp/hdfs_completer/$CLUSTER_NAME"
 if ! test -S $SOCKET_PATH; then
     echo >&2 "Hdfs completer server is down. Launching..."
 
-    CLUSTER_URL=$(cat $HOME/.hdfs_completer/clusters/$CLUSTER_NAME)
+    readonly CLUSTER_URL_FILE=$HOME/.hdfs_completer/clusters/$CLUSTER_NAME
+    if ! test -f $CLUSTER_URL_FILE; then
+        echo >&2 "File $CLUSTER_URL_FILE not found. Please populate it with a URL to $CLUSTER_NAME"
+        exit 1
+    fi
+
+    CLUSTER_URL=$(cat $CLUSTER_URL_FILE)
+
     readonly LOG_PATH="/tmp/hdfs_completer/logs/$CLUSTER_NAME"
     mkdir -p /tmp/hdfs_completer/logs
     $SCRIPT_ROOT/env/bin/python $SCRIPT_ROOT/completer.py --hdfs_host=$CLUSTER_URL --logging=debug --use_kerberos --unix_socket=$SOCKET_PATH --log-file-prefix=$LOG_PATH &
